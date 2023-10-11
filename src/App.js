7823e705251f03b08d5448e0f66f35563df24c54
Udylia <udylia@yandex.ru>
2023-10-11 11:19:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { slides } from "./slides";
+import { data } from "./data";
 import './App.css';
 
 function App() {
+
+  const [slide, setSlide] = useState(0);
+  const {image} = slides [slide];
+  const [toDo, setToDo] = useState(data);
+  const removeItem = (id) => {
+    let newArray = toDo.filter((item) => item.id !== id);
+    setToDo(newArray);
+
+  }
+
+  const prevImage = () => {
+    setSlide ((slide => {
+      slide --;
+      if (slide < 0){
+        return slides.length -1;
+      }
+      return slide;
+    }))
+  }
+
+  const nextImage = () => {
+    setSlide ((slide => {
+      slide ++;
+      if (slide > slides.length -1){
+        slide =0;
+      }
+      return slide;
+    }))
+  }
+ 
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>Список из {toDo.length} дел</h1>
+      </div>
+      <div className='container'>
+        <img src={image} width="400px"  alt="Todo"/>
+      </div>
+      <div className='container'>
+      <button className="btn" onClick={prevImage}>&#8592;Предыдущий</button>
+      <button className="btn" onClick={nextImage}>Следующий&#8594;</button>
+      </div>
+     
+      {toDo.map((element => {
+        const {id, toDoData} = element;
+        return (
+          <div key={id}>
+            <div className='container'>
+            <p>{id} - {toDoData}</p>
+            </div>
+            <div className="container">
+            <button className="btn" onClick ={() => removeItem(id)}>Удалить из списка</button>
+            </div>
+          </div>
+        )
+      }))}
+      <div className='container'>
+        <button className="button" onClick ={() => setToDo([])}>Очистить весь список</button>
+      </div>
+      
+      
+      
     </div>
-  );
+  )
+    
 }
 
 export default App;
---
+++ src/slides.js
@@ -0,0 +1,33 @@
+import toDo from './todotwo.jpg';
+import beauty from './beauty.jpg';
+import sport from './sport.jpg';
+import hockey from './hockey.jpg';
+import it from './it.jpg';
+import shopping from './shopping.jpg';
+
+export const slides =[
+   {
+    id: 1,
+    image: toDo,
+   },
+   {
+    id: 2,
+    image: beauty,
+   },
+   {
+    id: 3,
+    image: sport,
+   },
+   {
+    id: 4,
+    image: hockey,
+   },
+   {
+    id: 5,
+    image: it,
+   },
+   {
+    id: 6,
+    image: shopping,
+   }
+]
